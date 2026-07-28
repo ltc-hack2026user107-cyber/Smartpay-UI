422cec143cf594329c74608d204f10b9dbe663e1
ltc-hack2026user109-tech <ltc-hack2026user109@green-tartan1.workshop.ongcp.co>
2026-07-28 11:03:19 +0530

    Update and rename next.config.ts to next.config.js
    converted ts to js config file

---
+++ next.config.js
@@ -0,0 +1,14 @@
+const nextConfig = {
+
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.pravatar.cc',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
